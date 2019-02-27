fa7f007b4412aa0c28f1d65997cb4d09ff1c3a2f
ZacharyLloyd <46659989+ZacharyLloyd@users.noreply.github.com>
2019-02-26 19:25:44 -0700

    UI Added

---
+++ Sneaky McSneak Face/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public Image healthUI;
+    public float currentHealth;
+    public float maxHealth;
+    public float damage;
+    public float ammo;
+    public float maxAmmo;
+    public TextMeshProUGUI ammoUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthUI.fillAmount = currentHealth / maxHealth;
+        ammo = maxAmmo;
+        ammoUI.text = "Ammo: " + ammo.ToString() + "/" + maxAmmo.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+            {
+                healthUI.fillAmount -= damage / maxHealth;
+            }
+    }
+}
