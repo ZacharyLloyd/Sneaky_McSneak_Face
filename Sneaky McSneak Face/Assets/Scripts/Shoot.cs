55b17246b21096631f381a8475fcca420a5b015c
ZacharyLloyd <46659989+ZacharyLloyd@users.noreply.github.com>
2019-02-28 16:35:17 -0700

    added shooting

--- Sneaky McSneak Face/Assets/Scripts/Shoot.cs
+++ Sneaky McSneak Face/Assets/Scripts/Shoot.cs
@@ -26,13 +26,13 @@ public class Shoot : MonoBehaviour
                         isKeyReleased = false;
                         //Bullet will spawn with a set direction based on player's direction
                         Instantiate(bulletPrefab, pointOfFire.position, pointOfFire.rotation);
-                        FindObjectOfType<audioManager>().Play("Shoot");
+                        FindObjectOfType<AudioManager>().Play("Shoot");
                         break;
                     case true:
                         isKeyReleased = false;
                         //Bullet will spawn with a set direction based on player's direction
                         Instantiate(bulletPrefab, pointOfFire.position, pointOfFire.rotation);
-                        FindObjectOfType<audioManager>().Play("Shoot");
+                        FindObjectOfType<AudioManager>().Play("Shoot");
                         StartCoroutine(coroutine);
                         break;
                 }
